fab3f0816afe99a6c6f80eac48b7b72c6f0d5d20
EdenWalker <nsknjh@gmail.com>
2024-06-27 18:52:38 +0800

    moved all html to html added photos

---
+++ timer.js
@@ -0,0 +1,7 @@
+// const para = document.getElementById("para");
+
+// function myMessage() {
+//   para.innerHTML = "I just appeared";
+//   console.log("message appeared");
+// }
+// setTimeout(myMessage, 0);
